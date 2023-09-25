db7289677deb49c2a6c9c69fc74d5a0040710235
evag2582 <elio.g28@gmail.com>
2023-09-25 12:28:00 -0400

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const createItem = (item) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+};
+
+const itemsContainer = document.querySelector('.gallery');
+for (const item of galleryItems) {
+    const newItem = createItem(item);
+    itemsContainer.innerHTML += newItem;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
