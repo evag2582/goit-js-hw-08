d1a5e662e1a899d168113b84b4fc4173295d2bf9
evag2582 <elio.g28@gmail.com>
2023-09-25 22:17:08 -0400

    update task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    let iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const throttle = require('lodash.throttle');
+    
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function(data) {
+        const timeUpdate = data.seconds; 
+        
+    localStorage.setItem("videoplayer-current-time", timeUpdate);
+ };
+player.on('timeupdate', onPlay);
+ 
+const timeUpdateRegister = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(timeUpdateRegister).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    player.play();
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
