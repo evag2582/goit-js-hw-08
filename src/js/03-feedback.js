7c02bfa23b26fc5ff6d384e380b2b83b45d3cbf5
evag2582 <elio.g28@gmail.com>
2023-09-29 16:57:52 -0400

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const input = document.querySelector('.feedback-form');
+var _ = require('lodash.throttle');
+
+  input.addEventListener("input", (e) => {
+    e.preventDefault();
+    const {
+      elements: { email, message }
+    } = e.currentTarget;
+    const emailAndMessage = {
+      email: email.value,
+      message: message.value
+    }  
+    
+    input.addEventListener("input", throttle(() => {
+      localStorage.setItem('feedback-form-state', JSON.stringify(emailAndMessage));
+    }, 500));
+ 
+  });
+
+input.addEventListener("submit", (e) => {
+  const savedJson = localStorage.getItem('feedback-form-state');
+  const saved = JSON.parse(savedJson);
+  
+  input.elements.email.value = saved.email ?? "";
+  input.elements.message.value = saved.message ?? "";
+  e.preventDefault();
+  console.log(saved);
+   localStorage.removeItem('feedback-form-state');
+   input.reset();
+ });
